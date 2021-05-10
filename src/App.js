59797a31a6d9a61d3004b4201006393133340464
Hemachandrasekar <46589633+Hemachandrasekar@users.noreply.github.com>
2021-05-10 16:47:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,21 +5,36 @@ import { Sidebar } from "./component/Sidebar";
 import "./App.css";
 
 const App = () => {
-  const [fetchSapcedata, setSpaceData] = useState();
+  const [spaceData, setSpaceData] = useState();
+  const [yearFilter, setYearFilter] = useState([]);
   useEffect(() => {
     fetch(
       `https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=true&amp;launch_year=2014`
-    ).then((res) => setSpaceData(res.data));
-    console.log("fetchSapcedata", fetchSapcedata);
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setSpaceData(data);
+        // setYearFilter(data.launch_year);
+      });
   }, []);
-
+  useEffect(() => {
+  
+  }, [spaceData]);
+  // const handleYear = () => {
+  //   setYearFilter(() => {
+  //     spaceData&&spaceData.map((item, index) => {
+  //       return item.launch_year;
+  //     });
+  //   });
+  //   // const myArray = [1, 2, 3, 1, 5, 8, 1, 2, 9, 4];
+  //   // const unique = [...new Set(myArray)]; // [1, 2, 3, 5, 8, 9, 4]
+  //   // console.log("unqiue value", unique);
+  //   // console.log("fetch yearfilter", yearFilter);
+  // };
   return (
-    <div>
-      {fetchSapcedata.map((item) => (
-        <div>{item.mission_name}</div>
-      ))}
-      <Sidebar />
-      <Maincontent />
+    <div className="grid grid-cols-4">
+      <Sidebar data={spaceData} />
+      <Maincontent  data={spaceData} />
     </div>
     // <div class="max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-20">
     //   <div class="flex justify-center md:justify-end -mt-16">
